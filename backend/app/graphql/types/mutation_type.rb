68e1139d054f9f7015a5ff576212dace659fa273
LIM-02 <limzhicheng2216@gmail.com>
2025-11-05 09:40:58 +0800

    Add UpdateItem mutation to manage item updates in GraphQL

--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_item, mutation: Mutations::Items::CreateItem
+    field :update_item, mutation: Mutations::Items::UpdateItem
   end
 end
